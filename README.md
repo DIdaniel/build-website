d7364007d29e1ac0e3e3d8c76a91b2887e2bba98
KIM DONG IL <dongil.kim@powertask.biz>
2022-03-03 18:13:16 +0900

    applied font style

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 [ref]
 https://www.youtube.com/watch?v=bSMZgXzC9AA&t=450s
-first: 27:06
+first: 34:17
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue,
+} from '@chakra-ui/react'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderradius="lg" bg="red" mb={6} p={3} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('WhiteAlpha.500', 'WhiteAlpha.200')}
+        mb={6}
+        p={3}
+        align="center"
+      >
         Hello, I&apos;m a frontend developer based in Seoul, Korea!😎
       </Box>
       <Box display={{ md: 'flex' }}>
@@ -12,6 +24,23 @@ const Page = () => {
             KIM DONG IL
           </Heading>
           <p>Digital Cratfzman ( Artist / Developer / Designer )</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="WhiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display=" inline-block"
+            borderRadius="full"
+            src="/images/takuya.jpg"
+            alt="KIM DONG IL"
+          />
         </Box>
       </Box>
     </Container>
